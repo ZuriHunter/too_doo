5affe66520f2614a6a02b80beb5415bf18ec33ef
ZuriHunter <zurihunter92@gmail.com>
2015-07-21 20:13:47 +0000

    Setup Mark as Complete feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
-    #nested within or to show conjunction to..url reads todo_lists/1/todo_items/1 etc...
+    resources :todo_items do
+       #nested within or to show conjunction to..url reads todo_lists/1/todo_items/1 etc...
+      member do
+        patch :complete
+      end
+    end
   end 
   
   root 'todo_lists#index'
